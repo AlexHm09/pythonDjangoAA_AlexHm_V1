a0e9c7c7f2ea69d13be0b2b4f457dcf12a5f3d3d
AlexHm09 <khmelevskii.aa@phystech.edu>
2024-05-20 04:03:09 +0300

    Сделал секретный Токен в .env Изменил описание на базовой странце (логин и пароль) Администратора

--- django_V1_AA/settings.py
+++ django_V1_AA/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os.path
+
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fvys!5)&)9xz84umg#gpeq3%!(ukk=1sw2es64!85gvr!2i88%'
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv()
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
